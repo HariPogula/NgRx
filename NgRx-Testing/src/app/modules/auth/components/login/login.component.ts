c187f3bea63a51a59d31cc456185723e8f5c565e
Hari <hariar2.0@outlook.com>
2020-05-26 17:24:59 -0600

    Added Auth reducers and actions

--- NgRx-Testing/src/app/modules/auth/components/login/login.component.ts
+++ NgRx-Testing/src/app/modules/auth/components/login/login.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/reducers';
+import { AuthService } from '../../services/auth.service';
+import { tap } from 'rxjs/operators';
+import { login } from '../../auth.actions';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private store: Store<AppState>,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
   }
 
+  login() {
+    const result = this.loginForm.value;
+    console.log('Result is ' + JSON.stringify(result));
+
+    this.store.dispatch(login({ user: result }));
+    // this.authService.login(result).pipe(
+    //   tap((u) => {
+    //     console.log('Tap User is ' + u);
+
+    //   })
+    // );
+  }
 }
--- NgRx-Testing/src/app/reducers/index.ts
+++ NgRx-Testing/src/app/reducers/index.ts
@@ -3,19 +3,16 @@ import {
   ActionReducerMap,
   createFeatureSelector,
   createSelector,
-  MetaReducer
+  MetaReducer,
 } from '@ngrx/store';
 import { environment } from '../../environments/environment';
 
 export const appFeatureKey = 'app';
 
-export interface State {
+export interface AppState {}
 
-}
+export const reducers: ActionReducerMap<AppState> = {};
 
-export const reducers: ActionReducerMap<State> = {
-
-};
-
-
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AppState>[] = !environment.production
+  ? []
+  : [];
